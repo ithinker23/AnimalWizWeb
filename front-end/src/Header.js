65f708a12f26e1fe34fd10e2d980a4f1f95e9bad
ithinker23 <44293948+ithinker23@users.noreply.github.com>
2023-02-06 02:24:49 -0800

    idk

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -1,11 +1,15 @@
+import { AiOutlineHome } from 'react-icons/ai'
+
+
 export default function Header() {
     return (<div className="header">
-        <div className='logo headerLink' onClick={()=>{window.location.href = "/"}}>
-            <img src="a_wizlogo.webp" alt='img' /></div>
+        <div className='logo headerLink' onClick={() => { window.location.href = "/" }}>
+            < AiOutlineHome size={30}/>
+        </div>
 
         <div className='headerLinkGroup'>
-            <div className='headerLink' onClick={()=>{window.location.href = "/findItems"}}>Item Selection</div>
-            <div className='headerLink' onClick={()=>{window.location.href = "/priceOptimization"}}>Price Optimization</div>
+            <div className='headerLink' onClick={() => { window.location.href = "/findItems" }}>Item Selection</div>
+            <div className='headerLink' onClick={() => { window.location.href = "/priceOptimization" }}>Price Optimization</div>
         </div>
     </div>)
 }
